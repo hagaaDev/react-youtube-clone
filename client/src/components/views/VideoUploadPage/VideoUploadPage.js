4c6be8db40c989cddd7bd9f795889788d3a7a91d
김선희 <tjsgml951012@gmail.com>
2022-02-16 10:39:24 +0900

    feat: VideoUploadPage 컴포넌트 내부 UI

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,5 +1,51 @@
 import React from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
+
+const { TextArea } = Input;
+const { Title } = Typography;
 
 export default function VideoUploadPage() {
-  return <div>VideoUploadPage</div>;
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+
+      <Form onSubmit>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          {/* Drop Zone */}
+
+          {/* Thumbnail */}
+          <div>
+            <img src alt />
+          </div>
+        </div>
+
+        <br />
+        <br />
+        <label>Title</label>
+        <Input onChange value />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea onChange value />
+        <br />
+        <br />
+
+        <select onChange>
+          <option key value></option>
+        </select>
+        <br />
+        <br />
+        <select onChange>
+          <option key value></option>
+        </select>
+        <br />
+        <br />
+        <Button type="primary" size="large" onClick>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
