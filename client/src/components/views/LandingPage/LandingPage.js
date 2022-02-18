4ddef50ee79d28d6a8787df10576449b6565f413
sunkim <sunkim@hist.co.kr>
2022-02-18 11:02:57 +0900

    feat: DB의 video데이터 API로 가져오기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,45 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FaCode } from "react-icons/fa";
 import { Card, Icon, Avatar, Col, Typography, Row } from "antd";
+import moment from "moment";
 const { Title } = Typography;
 const { Meta } = Card;
 
 function LandingPage() {
-  //const renderCards =
+  // mongoDB에서 video데이터 가져오기
+
+  const [Video, setVideo] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/video/getVideos").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setVideo(response.data.videos);
+      } else {
+        alert("비디오 가져오기를 실패 했습니다.");
+      }
+    });
+  }, []);
+
+  const renderCards = Video.map((video, index) => {
+    var minutes = Math.floor(video.duration / 60);
+    var seconds = Math.floor(video.duration - minutes * 60);
+
+    return (
+      <Col key={index} lg={6} md={8} xs={2}>
+        <a href={`/video/post/${video._id}`}>
+          <div style={{ position: "relative" }}>
+            {
+              <img
+                style={{ width: "100%" }}
+                src={`http://localhost:5000/${video.thumbnail}`}
+                alt="thumbnail"
+              />
+            }
+            <div className="duration">
+              <span>
+                {minutes} : {seconds}
+              </span>
+            </div>
+          </div>
+        </a>
+        <br />
+        <Meta
+          avatar={<Avatar src={video.writer.image} />}
+          title={video.title}
+          description=""
+        />
+        <span>{video.writer.name}</span>
+        <br />
+        <span style={{ marginLeft: "3rem" }}>{video.views} views</span> -
+        <span>{moment(video.createdAt).format("MMM do YY")}</span>
+      </Col>
+    );
+  });
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
       <Title level={2}> Recommended </Title>
       <hr />
-      <Row gutter={[32, 16]}>
-        {/* {renderCards}
-        <Col key={index} lg={6} md={8} xs={2}>
-          <a href={`/video/post/${video._id}`}>
-          <div style={{ position: "relative" }}>
-            {<img
-                 style={{ width: "100%" }}
-                src={`http://localhist:5000/${video.thumbnail}`}
-                alt="thumbnail"
-              />}
-            <div className="duration">
-              <span>
-                  {minutes} : {seconds}
-                </span>
-            </div>
-          </div>
-          </a>
-          <br />
-          <Meta
-            avartar={<Avartar src={video.writer.image} />}
-            title={video.title}
-            description=""
-          />
-          <span>{video.writer.name}</span>
-          <br />
-          <span style={{ marginLeft: "3rem" }}>{video.views} views</span> -
-          <sapn>{moment(video.createdAt).format("MMM do YY")}</sapn>
-        </Col> */}
-      </Row>
+      <Row gutter={[32, 16]}>{renderCards}</Row>
     </div>
   );
 }
