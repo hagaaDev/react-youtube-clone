aca381d5254ed5c82e1c2dce9a176d418fbd6dd0
sunkim <sunkim@hist.co.kr>
2022-02-18 09:32:07 +0900

    feat: LandingPage UI

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
 import { FaCode } from "react-icons/fa";
+import { Card, Icon, Avatar, Col, Typography, Row } from "antd";
+const { Title } = Typography;
+const { Meta } = Card;
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+  //const renderCards =
+  return (
+    <div style={{ width: "85%", margin: "3rem auto" }}>
+      <Title level={2}> Recommended </Title>
+      <hr />
+      <Row gutter={[32, 16]}>
+        {/* {renderCards}
+        <Col key={index} lg={6} md={8} xs={2}>
+          <a href={`/video/post/${video._id}`}>
+          <div style={{ position: "relative" }}>
+            {<img
+                 style={{ width: "100%" }}
+                src={`http://localhist:5000/${video.thumbnail}`}
+                alt="thumbnail"
+              />}
+            <div className="duration">
+              <span>
+                  {minutes} : {seconds}
+                </span>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+          </div>
+          </a>
+          <br />
+          <Meta
+            avartar={<Avartar src={video.writer.image} />}
+            title={video.title}
+            description=""
+          />
+          <span>{video.writer.name}</span>
+          <br />
+          <span style={{ marginLeft: "3rem" }}>{video.views} views</span> -
+          <sapn>{moment(video.createdAt).format("MMM do YY")}</sapn>
+        </Col> */}
+      </Row>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
