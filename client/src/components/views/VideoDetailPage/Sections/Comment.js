dbe55c969660981d0fbefa61d826ad1446a59d9b
sunkim <sunkim@hist.co.kr>
2022-02-21 17:01:13 +0900

    #14 feat: Comment.js UI 및 post API 작성

--- client/src/components/views/VideoDetailPage/Sections/Comment.js
+++ client/src/components/views/VideoDetailPage/Sections/Comment.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import { useSelector } from "react-redux";
 
 export default function Comment() {
-  return <div>Comment</div>;
+  const videoId = props.match.params.videoId;
+  const user = useSelector((state) => state.user); // 리덕스를 이용하여 로그인유저 정보를 가져옴
+  const [commentValue, setCommentValue] = useState("");
+  const handleClick = (event) => {
+    setCommentValue(event.currentTarget.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const variables = {
+      content: commentValue,
+      writer: user.data._id,
+      postId: videoId,
+    };
+    Axios.post("/api/comment/saveComment", variables).then((response) => {
+      if (response.data.success) {
+      } else {
+        alert("코멘트를 저장하지 못했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div>
+      <br />
+      <p> Replies</p>
+      <hr />
+
+      {/* Comment Lists */}
+
+      {/* Root Comment Form */}
+
+      <form style={{ display: "flex" }} onSubmit={onSubmit}>
+        <textarea
+          style={{ width: "100%", borderRadius: "5px" }}
+          onChange={handleClick}
+          value={commentValue}
+          placeholder="코멘트를 작성해 주세요"
+        />
+        <br />
+        <button style={{ width: "20%", height: "52px" }} onClick={onSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
