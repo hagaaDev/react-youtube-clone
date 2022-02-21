46dd06c19a853f8e5ed4114fafff72a5d81ce35f
sunkim <sunkim@hist.co.kr>
2022-02-21 14:28:51 +0900

    fix: : subscribedUser 500에러 해결

--- client/src/components/views/SubscriptionPage/SubscriptionPage.js
+++ client/src/components/views/SubscriptionPage/SubscriptionPage.js
@@ -63,7 +63,7 @@ export default function SubscriptionPage() {
   });
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
-      <Title level={2}> Recommended </Title>
+      <Title level={2}> Subscription </Title>
       <hr />
       <Row gutter={[32, 16]}>{renderCards}</Row>
     </div>
