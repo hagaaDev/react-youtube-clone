4f1bc4b9ca9daa93800a46d65d619249d85eec2a
sunkim <sunkim@hist.co.kr>
2022-02-21 15:31:47 +0900

    #14 feat: Comment.js 생성 및 VideoDetailPage의 Comment영역에 렌더링

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
 import SideVideo from "./Sections/SideVideo";
 import Subscribe from "./Sections/Subscribe";
+import Comment from "./Sections/Comment";
 
 export default function VideoDetailPage(props) {
   const videoId = props.match.params.videoId;
@@ -49,6 +50,7 @@ export default function VideoDetailPage(props) {
             </List.Item>
 
             {/* Comments */}
+            <Comment />
           </div>
         </Col>
         <Col lg={6} xs={24}>
